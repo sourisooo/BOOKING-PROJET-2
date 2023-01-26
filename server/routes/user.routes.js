5d8c1d547afb29686453d9a14723e3357ed27545
Sourisooo <s.doan@skema.edu>
2023-01-26 21:14:41 +0100

    first commit

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -34,3 +34,15 @@ router.patch('/:userId', auth, async (req, res) => {
 });
 
 module.exports = router;
+
+//Commentaires
+//La page de code gère toutes les requetes réalisées au server prenant racine à '/' et '/:userId',
+//concernant l'array d'objet user.
+//La requete GET prend pour option une fonction asynchrone prenant pour entrée une requete et un reponse.
+//Une recherche est faite sur touts les objets de l'array user, puis une réponse est envoyée au client
+//en retournant l'intégralité de l'array user.
+//La requete PATCHpour option une fonction asynchrone prenant pour entrée une requete et un reponse. L'objet
+//requete est destructuré à travers la méthode params pour accéder aux paramètres et variables.
+//Une recherche est réalisé sur l'array user, et les données sont modifiées sur l'objet trouvé par la
+//recherche, les nouvelles données étant insérées dans le corps de la méthode findByIdAndUpdate.
+//Une réponse est envoyée au client en envoyant l'objet précédemment trouvé et modifié.
